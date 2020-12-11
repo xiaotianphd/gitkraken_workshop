328de652bdcd3e6d531fe043a93e931011c5cd52
Xiao Tian <xiao_tian@hms.harvard.edu>
2020-12-11 11:07:32 -0500

    First set of R scripts added

--- code/scriptlets.R
+++ code/scriptlets.R
@@ -1,13 +1,24 @@
 #!/usr/bin/env Rscript
 
-# Put comments here
-#  including pseudo-code of program flow
+# This script will include a collection of small scripts steps
+# often seen as example code. We're using this solely for demo purposes
 
 # Put globals, installs, and sources here
+source("./util_functions.R")
 
 # Put functions here
+#   1. Prints hello world
+hello_world <- function() {
+  myString <- "Hello, World!"
+  print (myString)
+}
 
-# now your main code
+# main code
+
+hello_world()
+
+square_it(10)
+
+montecarloPi(3000)
 
 # END
-
